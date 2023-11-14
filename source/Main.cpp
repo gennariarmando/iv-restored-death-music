35ff148b6855e3b07c6d3b79bc2cd8cf72c52670
_AG <gennariarmando@outlook.com>
2023-11-14 22:26:54 +0100

    Use patterns dude

--- source/Main.cpp
+++ source/Main.cpp
@@ -7,14 +7,15 @@ using namespace plugin;
 class RestoredDeathMusicIV {
 public:
     static void TempHackyPlayDeathMusic(int32_t i) {
-        const char* prev = plugin::patch::Get<const char*>(0x8E4803 + 1);
+        static auto pattern = plugin::pattern::Get("68 ? ? ? ? 8B CF C7 44 24 ? ? ? ? ? E8 ? ? ? ? 83 3E 00 74 2A 6A FF 6A 01 68 ? ? ? ? E8 ? ? ? ? 8B 0E 83 C4 04 50 E8 ? ? ? ? FF 35 ? ? ? ? 8B 0D ? ? ? ? E8 ? ? ? ? 5E 5F 83 C4 48 C3 CC CC CC CC 83 EC 48", 1);
+        const char* prev = plugin::patch::Get<const char*>(pattern);
         char buf[32];
         sprintf(buf, "DEATH_MUSIC_%02d", i);
-        plugin::patch::Set<const char*>(0x8E4803 + 1, buf);
+        plugin::patch::Set<const char*>(pattern, buf);
 
         FrontendAudioEntity.StartLoadingTune();
 
-        plugin::patch::Set<const char*>(0x8E4803 + 1, prev);
+        plugin::patch::Set<const char*>(pattern, prev);
     }
 
     RestoredDeathMusicIV() {
